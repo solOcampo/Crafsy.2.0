1531e0e4bdf3fe4374ff880db5bb845003c379e1
Comision17 <comision17formar@gmail.com>
2022-08-04 09:16:36 -0300

    actualizo los cambios de la clase 52

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -11,6 +11,13 @@ module.exports = {
         return res.render('admin/crearProducto')
     },
     edit:(req,res) => {
-        return res.render('admin/editarProducto')
+        id = +req.params.id
+        let producto = productos.find((elemento) => {
+            return elemento.id == id
+        })
+        /* return res.send(producto) Comprobar que esta llegando bien el elemento*/
+        return res.render('admin/editarProducto',{
+            producto
+        })
     },
 }
--- src/routes/admin.js
+++ src/routes/admin.js
@@ -5,6 +5,6 @@ const router = express.Router();
 /* GET home page. */
 router.get('/list', list);
 router.get('/create', create);
-router.get('/edit', edit);
+router.get('/edit/:id', edit);
 
 module.exports = router;
